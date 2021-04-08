449fe307da9dbe3b5b551dddbaca9eaa9e9b91c8
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-04-08 08:37:25 -0500

    first commit

---
+++ login/views.py
@@ -0,0 +1,42 @@
+import os
+from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+
+from rest_framework import status, generics
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import RegisterUserSerializer
+# Create your views here.
+
+
+#View to put the refresh token in the blacklist when the user logs out
+class BlacklistTokenView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token) 
+            token.blacklist()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)   
+
+
+class CreateUser(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format='json'):
+        data = request.data
+        reg_serializer = RegisterUserSerializer(data=data)
+        if reg_serializer.is_valid():
+            password = reg_serializer.validated_data.get('password')
+            reg_serializer.validated_data['password']=make_password(password)
+            new_user = reg_serializer.save()
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(reg_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
